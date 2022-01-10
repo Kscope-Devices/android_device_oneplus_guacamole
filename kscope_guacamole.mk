01f66b9086d28f0d3ba13c7a8927c8369d57663b
TH779 <i@779.moe>
2022-01-10 14:33:51 +0800

    guacamole: Kaleidoscope is a dream
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I51f90f863bf303c66daf861a79a68376dba6e33b

--- kscope_guacamole.mk
+++ kscope_guacamole.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Kaleidoscope stuff.
+$(call inherit-product, vendor/kscope/config/mobile.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_guacamole
+PRODUCT_NAME := kscope_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
